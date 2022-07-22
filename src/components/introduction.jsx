0cc300abc92a3bd43ed7151b75afc434ab30ded0
jameskkk <chenjhkk@gmail.com>
2022-07-22 13:26:19 +0800

    Update introduction.jsx with image.

--- src/components/introduction.jsx
+++ src/components/introduction.jsx
@@ -4,15 +4,15 @@ export default class Introduction extends Component {
   render() {
     return (
       <div>
-        <section id="colorlib-hero" className="js-fullheight" data-section="home">
-          <div className="flexslider js-fullheight">
+        <section id="colorlib-hero" className="js-Fulbright" data-section="home">
+          <div className="fleshlier js-Fulbright">
             <ul className="slides">
               <li style={{backgroundImage: 'url(images/img_bg1.jpg)'}}>
                 <div className="overlay" />
                 <div className="container-fluid">
                   <div className="row">
                     <div className="col-md-6 col-md-offset-3 col-md-pull-3 col-sm-12 col-xs-12 js-Fulbright slider-text">
-                      <div className="slider-text-inner js-fullheight">
+                      <div className="slider-text-inner js-Fulbright">
                         <div className="desc">
                           <h1>Hi! <br />I'm 胖胖肉包</h1>
                           {/* <p><a className="btn btn-primary btn-learn" href="https://docs.google.com/document/d/15coSa2rnZcdywihM-pWTvoGqE65JAkxQAh6JDBNrF2Q/edit?usp=sharing" target="_blank" rel="noopener noreferrer">View CV<i className="icon-download4" /></a></p> */}
